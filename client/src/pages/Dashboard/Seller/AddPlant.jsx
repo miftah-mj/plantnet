e8a19c8d2bbc0c2758946bc5c0bcfc0eef0a6f70
miftah-mj <jannat27120@gmail.com>
2025-01-20 23:21:10 +0600

    feat: implement plant addition functionality with image upload and seller info

--- client/src/pages/Dashboard/Seller/AddPlant.jsx
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,17 +1,58 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { uploadImage } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
 
 const AddPlant = () => {
-  return (
-    <div>
-      <Helmet>
-        <title>Add Plant | Dashboard</title>
-      </Helmet>
+    const { user } = useAuth();
+    const [uploadButton, setUploadButton] = useState("Upload Image");
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const description = form.description.value;
+        const category = form.category.value;
+        const price = parseFloat(form.price.value);
+        const quantity = parseInt(form.quantity.value);
+        const image = form.image.files[0];
+        const imageUrl = await uploadImage(image);
 
-      {/* Form */}
-      <AddPlantForm />
-    </div>
-  )
-}
+        // Seller Info
+        const seller = {
+            name: user.displayName,
+            image: user.photoURL,
+            email: user.email,
+        };
 
-export default AddPlant
+        // Create plant object
+        const plant = {
+            name,
+            description,
+            category,
+            price,
+            quantity,
+            image: imageUrl,
+            seller,
+        };
+        console.table(plant);
+    };
+
+    return (
+        <div>
+            <Helmet>
+                <title>Add Plant | Dashboard</title>
+            </Helmet>
+
+            {/* Form */}
+            <AddPlantForm
+                handleSubmit={handleSubmit}
+                uploadButton={uploadButton}
+                setUploadButton={setUploadButton}
+            />
+        </div>
+    );
+};
+
+export default AddPlant;
